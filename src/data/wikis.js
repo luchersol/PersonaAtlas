70d6119f84eaa8d08d4d841b9ea8a9526110819e
luchersol <lmherencia2003@gmail.com>
2026-06-30 20:44:54 +0200

    feat: add Astro framework and initial wiki selection page
    - Updated package.json to include Astro as a dependency and added root scripts for development, building, and previewing.
    - Added favicon.ico and favicon.svg for the application.
    - Created wikis.js to store wiki data for Persona games.
    - Developed index.astro as the main entry point, featuring a carousel for wiki selection with dynamic background colors.
    - Introduced TypeScript configuration for strict type checking and path mappings.

---
+++ src/data/wikis.js
@@ -0,0 +1,20 @@
+export const wikisData = [
+  // {
+  //   title: "Persona 3 Reload",
+  //   favicon: "./wikis/persona-3-reload/public/favicon.svg",
+  //   bg_color: "#0083ca",
+  //   link: "/wikis/persona-3-reload"
+  // },
+  {
+    title: "Persona 4 Golden",
+    favicon: "./wikis/persona-4-golden/public/favicon.svg",
+    bg_color: "#ffd700",
+    link: "/wikis/persona-4-golden"
+  },
+  // {
+  //   title: "Persona 5 Royal",
+  //   favicon: "./wikis/persona-5-royal/public/",
+  //   bg_color: "#da0000",
+  //   link: "/wikis/persona-5-royal"
+  // }
+];
